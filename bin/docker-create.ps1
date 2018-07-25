42a717d73b14d441f1eaab384b7d098ac7232cbd
Ryan Cook <rcook@redhat.com>
2018-07-25 20:32:49 +0000

    bad line x2

--- bin/docker-create.ps1
+++ bin/docker-create.ps1
@@ -2,8 +2,7 @@
 IF ($a -eq "True") {Write-Host "Docker Network Already Created";exit}
 Write-Host "Creating Docker Network"
 date > c:\k\docker_create.lock
-$roughname = Get-NetAdapter | where adminstatus -eq "up" | Format-List -Property "Name" | Out
--String
+$roughname = Get-NetAdapter | where adminstatus -eq "up" | Format-List -Property "Name" | Out-String
 $NetAdapterName = $roughname.Substring(11)
 # multinode
 #docker network create -d transparent --gateway 10.128.2.1 --subnet 10.128.2.0/24 -o com.docker.network.windowsshim.interface="Ethernet0" external
