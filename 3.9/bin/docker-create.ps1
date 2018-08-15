b1d259e6204ec6e3415e7b9cb4219ed234c53c63
Rajat Chopra <rchopra@redhat.com>
2018-08-14 17:28:57 -0700

    Change docker network arguments
    The subnet can actually be the entire global pod network itself - the Windows transparent VmSwitch that gets created does not care about the actual pods lying on this node or elsewhere.

--- 3.9/bin/docker-create.ps1
+++ 3.9/bin/docker-create.ps1
@@ -12,5 +12,5 @@ echo $INTERFACE_ALIAS > C:\k\interface.txt
 # multinode
 #docker network create -d transparent --gateway 10.128.2.1 --subnet 10.128.2.0/24 -o com.docker.network.windowsshim.interface="Ethernet0" external
 # Single node
-docker network create -d transparent --gateway 10.128.1.1 --subnet 10.128.1.0/24 -o com.docker.network.windowsshim.interface=$INTERFACE_ALIAS  external
+docker network create -d transparent --gateway 10.128.1.1 --subnet 10.128.0.0/14 -o com.docker.network.windowsshim.interface=$INTERFACE_ALIAS  external
 
