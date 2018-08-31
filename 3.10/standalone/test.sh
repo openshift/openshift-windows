7097b4e431ac30450634adb03fc1f3ca11d97389
glennswest <glennswest@neuralcloudcomputing.com>
2018-08-31 08:40:34 -0500

    Switch to blue cluster

--- 3.10/standalone/test.sh
+++ 3.10/standalone/test.sh
@@ -1,2 +1,2 @@
-./allinone.sh openshift winnode01 star.k.e2e.bos.redhat.com openshift.star.k.e2e.bos.redhat.com apps.star.k.e2e.bos.redhat.com glennswest SuperLamb1
+./allinone.sh openshift winnode01 blue.k.e2e.bos.redhat.com openshift.blue.k.e2e.bos.redhat.com apps.blue.k.e2e.bos.redhat.com glennswest SuperLamb1
 
