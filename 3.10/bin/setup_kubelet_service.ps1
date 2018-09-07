0e8118b2fb50f9b2cf96d2770d09e24c6300cb36
glennswest <glennswest@neuralcloudcomputing.com>
2018-09-07 09:33:58 +0800

    Fix service name

--- 3.10/bin/setup_kubelet_service.ps1
+++ 3.10/bin/setup_kubelet_service.ps1
@@ -1,14 +1,14 @@
 $powershell = (Get-Command powershell).Source
 $scriptPath = "C:\k\hybrid\3.10\bin\start_kubelet.ps1"
 $arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $scriptPath
-$serviceName = "OcpKubelet"
+$serviceName = "ocpkubelet"
 nssm install $serviceName $powershell $arguments
-nssm set kubelet DisplayName OcpKubelet
-nssm set kubelet Description Kubelet For OpenShift
-nssm set kubelet Start SERVICE_AUTO_START
-nssm set kubelet AppStdout C:\k\hybrid\logs\kubelet.log
-nssm set kubelet AppStderr C:\k\hybrid\logs\kubelet.log
-nssm set kubelet AppRotateFiles 1
-nssm set kubelet AppRotateOnline 1
-nssm set kubelet AppRotateSeconds 86400
-nssm set kubelet AppRotateBytes 1048576
+nssm set ocpkubelet DisplayName ocpkubelet
+nssm set ocpkubelet Description Kubelet For OpenShift
+nssm set ocpkubelet Start SERVICE_AUTO_START
+nssm set ocpkubelet AppStdout C:\k\hybrid\logs\kubelet.log
+nssm set ocpkubelet AppStderr C:\k\hybrid\logs\kubelet.log
+nssm set ocpkubelet AppRotateFiles 1
+nssm set ocpkubelet AppRotateOnline 1
+nssm set ocpkubelet AppRotateSeconds 86400
+nssm set ocpkubelet AppRotateBytes 1048576
