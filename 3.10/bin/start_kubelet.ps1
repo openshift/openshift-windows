e46907771b967bccd59ee753177712b3063a4a58
glennswest <glennswest@neuralcloudcomputing.com>
2018-09-07 08:59:54 +0800

    Fix pause container

--- 3.10/bin/start_kubelet.ps1
+++ 3.10/bin/start_kubelet.ps1
@@ -6,7 +6,7 @@ c:\bin\kubelet.exe --hostname-override="$Env:lhost" --v=6 `
     --cluster-dns="10.0.0.4,10.0.1.6" --cluster-domain=cluster.local `
     --kubeconfig=c:\k\config --hairpin-mode=promiscuous-bridge `
     --image-pull-progress-deadline=20m --cgroups-per-qos=false `
-    --enforce-node-allocatable="" --pod-infra-container-image=glennswest/pause:latest `
+    --enforce-node-allocatable="" --pod-infra-container-image=kubeletwin/pause:latest `
     --network-plugin=cni --cni-bin-dir="c:\bin" --cni-conf-dir "c:\cni" 
 
 
