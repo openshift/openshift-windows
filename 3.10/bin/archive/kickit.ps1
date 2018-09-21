71c47d3b1d469edc1ca1a0e7c2de1d68176a85a8
“gwest“ <gwest@redhat.com>
2018-09-21 05:25:09 -0500

    Cleanup for PR of 3.10

--- 3.10/bin/archive/kickit.ps1
+++
@@ -1,2 +0,0 @@
-\bin\kubelet.exe --hostname-override="msnode01" --kubeconfig \k\config --resolv-conf="" --cni-bin-dir=/bin --network-plugin=cni --cni-conf-dir=\k\hybrid\cni --pod-infra-container-image="kubeletwin/pause" --allow-privileged=true --v=6 --enforce-node-allocatable="" --cgroups-per-qos=false
-
