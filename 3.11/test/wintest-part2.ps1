06b058d30cb7221f62b90b1a4a1a087089b65756
glennswest <gwest@redhat.com>
2018-10-15 14:11:18 -0500

    Remove old files

--- 3.11/test/wintest-part2.ps1
+++
@@ -1,7 +0,0 @@
-choco install wget -y
-wget.exe https://cloudbase.it/downloads/openvswitch-hyperv-2.7.0-certified.msi -O openvswitch.msi
-msiexec /i openvswitch.msi ADDLOCAL="OpenvSwitchCLI,OpenvSwitchDriver,OVNHost" /qn
-NetSh Advfirewall set allprofiles state off
-setx PATH "C:\Program Files\Cloudbase Solutions\Open vSwitch\bin;$env:path" -m
-sleep 5
-Start-Service ovs-vswitchd
