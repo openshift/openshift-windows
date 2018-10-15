e40f73e0a12e114d89637584d1d9e8c1a156dfda
glennswest <gwest@redhat.com>
2018-10-15 10:06:17 -0500

    Tweak to resolve hung network

--- 3.11/bin/setup-network.ps1
+++ 3.11/bin/setup-network.ps1
@@ -4,18 +4,14 @@ Write-Host "Installing Network"
 date > c:\k\network_setup.lock
 $ErrorActionPreference = "SilentlyContinue"
 
-# The name is saved in the docker setup
-$roughname = Get-Content "C:\k\interface.txt" | Out-String
-$INTERFACE_ALIAS= $roughname -replace "`n|`r",""
-echo $INTERFACE_ALIAS
-
 Import-Module "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\HostNetworkingService\HostNetworkingService.psm1"
 Import-Module "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\OVS\OVS.psm1"
 Import-Module "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\HNSHelper\HNSHelper.psm1"
 
 # # There should be only one transparent network
-$HNS_NW = Get-HNSNetwork | where {$_.type -eq "transparent"}
+$HNS_NW = Get-OVSEnabledHNSNetworks
 $HNS_ID = $HNS_NW.Id
+$INTERFACE_ALIAS = $HNS_NW.NetworkAdapterName
 Disable-OVSOnHNSNetwork $HNS_ID
 ovs-vsctl --if-exists --no-wait del-br br-ex
 ovs-vsctl --no-wait --may-exist add-br br-ex
