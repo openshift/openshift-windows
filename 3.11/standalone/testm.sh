5f4bfa11c38d47fb529a5d5ab16bc2524283f38b
“gwest“ <gwest@redhat.com>
2018-11-05 01:09:39 -0600

    Move setup from multihost to setup.yml

--- 3.11/standalone/testm.sh
+++
@@ -1,2 +0,0 @@
-./multihost.sh master,node01,node02 winnode01,winnode02 bright.k.e2e.bos.redhat.com master.bright.k.e2e.bos.redhat.com apps.bright.k.e2e.bos.redhat.com glennswest SuperLamb1 gwest@redhat.com Shadow2018
-
